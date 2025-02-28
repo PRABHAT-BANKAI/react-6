5108bc0f3734eea82629b411a8e0b227aa29479d
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-02-28 18:26:50 +0530

    todolist

--- day7todolist/todolist/src/components/Todolist.jsx
+++ day7todolist/todolist/src/components/Todolist.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 
 const Todolist = () => {
   const [inputText, setInputText] = useState("");
+  const [boolean, setBoolean] = useState(false);
+  const [editIndex, setEditIndex] = useState("");
   const [todolist, setTodolist] = useState(
     JSON.parse(localStorage.getItem("todolistData")) || []
   );
@@ -11,9 +13,25 @@ const Todolist = () => {
     setInputText("");
   }
 
+  function handleDelete(index) {
+    let deleteData = todolist.filter((item, i) => i != index);
+    setTodolist(deleteData);
+  }
+  function handleEdit(index) {
+    setInputText(todolist[index]);
+    setBoolean(true);
+    setEditIndex(index);
+  }
+  function handleUpdate() {
+    let updateData = todolist.map((item, i) => (i == editIndex? inputText : item));
+    setTodolist(updateData);
+    setBoolean(false);
+    setInputText("");
+  }
+
   useEffect(() => {
     localStorage.setItem("todolistData", JSON.stringify(todolist));
-  },[todolist]);
+  }, [todolist]);
   // console.log(todolist)
   // controlled component uncontrolled component
   return (
@@ -26,13 +44,17 @@ const Todolist = () => {
         onChange={(event) => setInputText(event.target.value)}
       />
 
-      <button onClick={handleAdd}>Add Task</button>
+      {boolean ? (
+        <button onClick={handleUpdate}>update</button>
+      ) : (
+        <button onClick={handleAdd}>Add Task</button>
+      )}
       <div>
         {todolist.map((todo, index) => (
           <div key={index}>
             <p>{todo}</p>
-            <button>Delete</button>
-            <button>Edit</button>
+            <button onClick={() => handleDelete(index)}>Delete</button>
+            <button onClick={() => handleEdit(index)}>Edit</button>
           </div>
         ))}
       </div>
