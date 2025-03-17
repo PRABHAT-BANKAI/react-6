3454eca3b1755ae9d0e3a253b650ae54210fa441
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-03-17 18:53:02 +0530

    searching

--- revision/product/src/components/Home.jsx
+++ revision/product/src/components/Home.jsx
@@ -4,7 +4,46 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+
   const [dark, setDark] = useState(false);
+  const [search, setSearch] = useState("");
+
+  function handleCategory(e) {
+    console.log(e.target.value);
+    if (e.target.value === "all") {
+      axios.get("http://localhost:3004/products").then((res) => {
+        setProducts(res.data);
+      });
+    } else {
+      let filterCategory = products.filter(
+        (prod) => prod.category === e.target.value
+      );
+      setProducts(filterCategory);
+    }
+  }
+
+  let searchData = products;
+
+  function handleSearch() {
+    let filterData = searchData.filter((product) =>
+      product.name.includes(search)
+    );
+    setProducts(filterData);
+    if (search == "") {
+      axios.get("http://localhost:3004/products").then((res) => {
+        setProducts(res.data);
+      });
+    }
+  }
+
+  function handleSort() {
+    let sortData = products.sort((a, b) => a.price - b.price);
+    setProducts([...sortData]);
+  }
+  function handleSort2() {
+    let sortData = products.sort((a, b) => b.price - a.price);
+    setProducts([...sortData]);
+  }
 
   function handleDelete(id) {
     axios
@@ -28,9 +67,30 @@ const Home = () => {
     });
   }, []);
   return (
-    <div style={{backgroundColor:dark?"black":"white", color:dark?"white":"black"}}>
+    <div
+      style={{
+        backgroundColor: dark ? "black" : "white",
+        color: dark ? "white" : "black",
+      }}
+    >
       <h1>Product page</h1>
+      <select onChange={handleCategory} name="" id="">
+        <option value="all">All</option>
+        <option value="slipper">Slipper</option>
+        <option value="sports">Sports</option>
+        <option value="shoes">Shoes</option>
+      </select>
+      <button onClick={handleSort}>low to high </button>
+      <button onClick={handleSort2}>high to low </button>
+      <input
+        type="text"
+        placeholder="searching"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button onClick={handleSearch}>search</button>
       <button onClick={() => setDark(!dark)}>Dark mode</button>
+
       <Link to={"/createProduct"}>
         <button>Create product </button>
       </Link>
@@ -46,6 +106,7 @@ const Home = () => {
           >
             <p>Product Name :{product.name}</p>
             <p>Price:{product.price} </p>
+            <p>Category :{product.category}</p>
             <button
               onClick={() => {
                 handleDelete(product.id);
